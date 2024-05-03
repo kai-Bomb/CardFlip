175bd9330be32196fd0c5a089a8d356d61120426
Aoitoridayo <aoitoridayo1110@icloud.com>
2024-05-03 20:52:54 +0900

    Initial Commit

---
+++ CardFlip/CardFlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardFlipApp.swift
+//  CardFlip
+//
+//  Created by 渡邊魁優 on 2024/05/03.
+//
+
+import SwiftUI
+
+@main
+struct CardFlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
